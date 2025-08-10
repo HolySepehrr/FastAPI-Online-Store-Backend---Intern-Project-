a382377e679379f17086f5444f8667e613cf61d5
Sepehr Fallah <Sepehr.Fallah@gmail.com>
2025-08-10 16:44:24 +0330

    4 choob bandi proje

--- API project/code_package/main.py
+++ API project/code_package/main.py
@@ -10,8 +10,36 @@ class Item(BaseModel):
     category: str
     stock: int = Path(ge=0)
 
+@app.post("/item")
+def add_item_endpoint(item: Item):
+    pass
 
-@app.get("/test")
-def root(item: Item):
-    return item
 
+@app.get("/items")
+def get_all_items(item: Item):
+    pass
+
+@app.get("/items/{item_id}")
+def get_single_item(item: Item, item_id: int):
+    pass
+
+@app.put("/item/{item_id}")
+def update_item_endpoint(item: Item):
+    pass
+
+
+@app.delete("/item/{item_id}")
+def remove_item_endpoint(item: Item):
+    pass
+
+@app.post("/cart/add")
+def add_item_to_cart_endpoint(item: Item):
+    pass
+
+@app.get("/cart")
+def get_cart_contents_endpoint(item: Item):
+    pass
+
+@app.delete("/cart/items/{item_id}")
+def remove_item_from_cart_endpoint(item: Item):
+    pass
